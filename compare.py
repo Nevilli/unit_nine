ecce59a068cdcc5570e8fdb824d1efee669d2faf
Lliam <lliam@iMac8.local>
2018-11-29 10:15:09 -0500

    Made draw card and main function

--- compare.py
+++ compare.py
@@ -1,10 +1,28 @@
 import deck
 
 
-def player_cards():
-    player_1_cards = []
-    player_2_cards = []
+def make_a_deck():
+    deck_of_cards = deck.Deck()
+    deck_of_cards.shuffle()
+    return deck_of_cards
 
 
-def deal_cards():
+def draw_a_card(deck_of_cards):
+    player_cards = []
+    for cards in range(5):
+        player_cards.append(deck_of_cards.draw_a_card())
+    return player_cards
+
+
+def compare(card1, card2):
+    return card1.compared_to(card2)
     
+
+
+def main():
+    deck_of_cards = make_a_deck()
+    player1 = draw_a_card(deck_of_cards)
+    player2 = draw_a_card(deck_of_cards)
+
+
+main()
