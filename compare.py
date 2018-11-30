af78cfdf7f44bae161588ac779513236d791a678
Lliam <lliam@iMac8.local>
2018-11-30 11:18:17 -0500

    Finished compare function

--- compare.py
+++ compare.py
@@ -18,11 +18,21 @@ def compare(card1, card2):
     return card1.compared_to(card2)
     
 
-
 def main():
     deck_of_cards = make_a_deck()
     player1 = draw_a_card(deck_of_cards)
     player2 = draw_a_card(deck_of_cards)
+    for x in range(5):
+        com = compare(player1[x], player2[x])
+        print("Player 1 drew a", player1[x])
+        print("Player 2 drew a", player2[x])
+        print(" ")
+        if com == player1[x]:
+            print("Player 1 wins!")
+            print()
+        elif com == player2[x]:
+            print("Player 2 wins!")
+            print()
 
 
 main()
